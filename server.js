78c49fdefd5b29c1af183fda670864228e61d30d
hsyntes <se.hsyntes@gmail.com>
2023-09-13 07:30:13 +0300

    update websocket

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ server.listen(process.env.PORT, () =>
 
       connection.on("message", (message) => {
         const { type, videoURL, title } = JSON.parse(message);
-        console.log(activeTab, videoURL, localized.title);
+        console.log(type, videoURL, title, connection);
         downloadVideo(type, videoURL, title, connection);
       });
     });
